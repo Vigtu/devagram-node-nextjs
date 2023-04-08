3f262fbb48d49f40eb63bbfa0a2ac91be790ac99
VICTOR CORREA GOMES <victor.cgomes@uvvnet.com.br>
2023-04-08 00:40:57 -0300

    JWT token Autentificação

--- models/usuarioModels.ts
+++ models/usuarioModels.ts
@@ -1,20 +1,29 @@
-import mongoose, {Schema} from 'mongoose';
+import mongoose, { Schema, Model } from 'mongoose';
 
-const usuarioSchema = new Schema({
-    nome: {type : String, required : true},
-    email: {type : String, required : true},
-    senha: {type : String, required : true},
-    avatar : {type : String, required : false},
-    seguidores : {type : Number, default : 0},
-    seguindo : {type : Number, default : 0},
-    publicacoes : {type : Number, default : 0},
+interface Usuario {
+  nome: string;
+  email: string;
+  senha: string;
+  avatar?: string;
+  seguidores?: number;
+  seguindo?: number;
+  publicacoes?: number;
+}
+
+const usuarioSchema = new Schema<Usuario>({
+  nome: { type: String, required: true },
+  email: { type: String, required: true },
+  senha: { type: String, required: true },
+  avatar: { type: String },
+  seguidores: { type: Number, default: 0 },
+  seguindo: { type: Number, default: 0 },
+  publicacoes: { type: Number, default: 0 },
 });
 
-
-let usuarioModel;
+let usuarioModel: Model<Usuario>;
 try {
-  usuarioModel = mongoose.model('usuarios');
+  usuarioModel = mongoose.model<Usuario>('usuarios');
 } catch (error) {
-  usuarioModel = mongoose.model('usuarios', usuarioSchema);
+  usuarioModel = mongoose.model<Usuario>('usuarios', usuarioSchema);
 }
 export { usuarioModel };
--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,20 +1,33 @@
 import type{NextApiRequest, NextApiResponse} from 'next';
 import {conectarMongoDB} from '../../../middlewares/conectaMongoDB';
 import type {repostaPadraoMsg} from '../../../types/respostaPadrao';
+import type {LoginResposta} from '../../../types/loginResposta';
 import md5 from 'md5';
 import {usuarioModel} from '../../../models/usuarioModels';
+import jwt from 'jsonwebtoken';
 
 const endpointLogin =  async (
     req: NextApiRequest,
-    res : NextApiResponse<repostaPadraoMsg>
+    res : NextApiResponse<repostaPadraoMsg | LoginResposta>
 ) => {
+
+    const{MINHA_CHAVE_JWT} = process.env;
+    if(!MINHA_CHAVE_JWT){
+        return res.status(500).json({error : 'ENV Jwt nao informada'});
+    }
+
     if(req.method === 'POST'){
         const {login, senha} = req.body
 
         const usuarioEcontrados = await usuarioModel.find({email : login, senha : md5(senha)})
         if(usuarioEcontrados && usuarioEcontrados.length > 0){
                 const usuarioEcontrado = usuarioEcontrados[0];
-                return  res.status(200).json({msg : `Usuario ${usuarioEcontrado.nome} autenticado com sucesso`});
+
+                const token = jwt.sign({_id : usuarioEcontrado._id}, MINHA_CHAVE_JWT);
+                return  res.status(200).json({
+                    nome : usuarioEcontrado.nome, 
+                    email : usuarioEcontrado.email, 
+                    token});
             }
             return res.status(405).json({error : 'Usuario ou senha não encontrados'});
     } 
