6deeb04364b3c5f1a1d9bfabf4dc676e0b39709e
VICTOR CORREA GOMES <victor.cgomes@uvvnet.com.br>
2023-04-15 05:06:29 -0300

    banco de dados de publicacao

--- src/pages/api/publicacao.ts
+++ src/pages/api/publicacao.ts
@@ -4,26 +4,45 @@ import nc from 'next-connect';
 import {upload, uploadImagemCosmic} from '../../../services/uploadImagemCosmic';
 import {conectarMongoDB} from '../../../middlewares/conectaMongoDB';
 import {validarTokenJwt} from '../../../middlewares/validarTokenJwt';
+import {publicacaoModel} from '../../../models/publicacaoModels';
+import {usuarioModel} from '../../../models/usuarioModels';
 
 const handler = nc()
     .use(upload.single('file'))
     .post(async (req : any, res : NextApiResponse<repostaPadraoMsg>) => {
         
         try{
+            const{userId} = req.query;
+            const usuario = await usuarioModel.findById(userId);
+            if(!usuario){
+                return res.status(400).json({error : 'Usuario não informado'})
+            }
+
+
             if(!req || !req.body){
                 return res.status(400).json({error : 'Parametros de entrada não informados'})
             }
             const {descricao} = req?.body;
 
-                if(!descricao || descricao.length < 2){
-                    return res.status(400).json({error : 'Descricao nao e valida'})
-                }
+            if(!descricao || descricao.length < 2){
+                return res.status(400).json({error : 'Descricao nao e valida'})
+            }
         
-                if(!req.file){
-                    return res.status(400).json({error : 'A imagem e obrigatoria'})
-                }
-        
-                return res.status(200).json({msg : 'A publicacao esta valida'})   
+            if(!req.file || !req.file.originalname){
+                 return res.status(400).json({error : 'A imagem e obrigatoria'})
+            }
+
+            const image = await uploadImagemCosmic(req);
+            const publicacao = {
+                idUsuario : usuario._id,
+                descricao,
+                foto : image.media.url,
+                data : new Date()
+    
+            }
+                
+            await publicacaoModel.create(publicacao);
+            return res.status(200).json({msg : 'A publicacao criada com sucesso '})   
         
         }catch(e){
             console.log(e); 
