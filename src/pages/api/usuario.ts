ac99b088caf6d678d04c6c3e48d4285c74c6de31
VICTOR CORREA GOMES <victor.cgomes@uvvnet.com.br>
2023-04-17 20:30:16 -0300

    endpoint de usuario com metodo de alteracao

--- src/pages/api/usuario.ts
+++ src/pages/api/usuario.ts
@@ -3,22 +3,66 @@ import type {repostaPadraoMsg} from '../../../types/respostaPadrao';
 import {validarTokenJwt} from '../../../middlewares/validarTokenJwt';
 import {conectarMongoDB} from '../../../middlewares/conectaMongoDB';
 import { usuarioModel } from "../../../models/usuarioModels";
+import nc from 'next-connect';
+import {upload, uploadImagemCosmic} from '../../../services/uploadImagemCosmic';
 
-const usuarioEndpoint = async (req : NextApiRequest, res : NextApiResponse<repostaPadraoMsg | any>) => {
+const handler = nc()
+    .use(upload.single('file'))
+    .put(async(req : any, res: NextApiResponse<repostaPadraoMsg>) => {
+        try{
+
+            const {userId} = req?.query;
+            const usuario = await usuarioModel.findById(userId);
+            if(!usuario){
+                return res.status(400).json({error : 'Usuario nao encontrado'});
+            }
+
+            const {nome} = req.body;
+            if(nome && nome.lenght < 2){
+                usuario.nome = nome;
+            }
+
+            const {file} = req;
+            if(file && file.originalname){
+
+                const image = await uploadImagemCosmic(req);
+                if(image && image.media && image.media.url){
+                    usuario.avatar = image.media.url;
+                }
+            }
+
+            // alterar os dados do DB
+            await usuarioModel
+                .findByIdAndUpdate({_id : usuario._id}, usuario);
+
+            return res.status(200).json({msg : 'Usuario alterado com sucesso'});
+
+        }catch(e){
+            console.log(e);
+            return res.status(400).json({error : 'Nao foi possivel atualizar o usuario' + e})
+        }
+    })
+    .get(async (req : NextApiRequest, res : NextApiResponse<repostaPadraoMsg | any>) => {
     
-    try{
-        // como pegar os dados do usuario logado
-        // id do usuario 
-        const {userId} = req?.query;
-        const usuario = await usuarioModel.findById(userId);
-        //usuario.senha = null;
-        return res.status(200).json(usuario);
+        try{
+            // como pegar os dados do usuario logado
+            // id do usuario 
+            const {userId} = req?.query;
+            const usuario = await usuarioModel.findById(userId);
+            //usuario.senha = null;
+            return res.status(200).json(usuario);
+    
+        }catch(e){
+            console.log(e);
+            return res.status(400).json({error : 'Nao foi possivel obter dados do usuario'})
+        }
+    
+    });
 
-    }catch(e){
-        console.log(e);
-        return res.status(400).json({error : 'Nao foi possivel obter dados do usuario'})
+    export const  config = {
+        api : {
+            bodyParser : false
+        }
     }
 
-}
-
-export default validarTokenJwt(conectarMongoDB(usuarioEndpoint));
+export default validarTokenJwt(conectarMongoDB(handler));
